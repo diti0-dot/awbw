b56b83c7c46a7a371cdb44b61cd55a4ffcc4d61b
diti <deepthinayak366@gmail.com>
2026-03-24 12:13:26 +0100

    Add start and end time to banner

---
+++ db/migrate/20260324110932_add_started_at_and_ended_at_to_banners.rb
@@ -0,0 +1,6 @@
+class AddStartedAtAndEndedAtToBanners < ActiveRecord::Migration[8.1]
+  def change
+    add_column :banners, :started_at, :datetime
+    add_column :banners, :ended_at, :datetime
+  end
+end
